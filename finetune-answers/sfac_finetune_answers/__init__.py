717e28988d9b6b717f6a4a13194e433356e9b9f8
John Shaffer <john@insilica.co>
2023-04-21 11:01:43 -0500

    Use argparse

--- finetune-answers/sfac_finetune_answers/__init__.py
+++ finetune-answers/sfac_finetune_answers/__init__.py
@@ -1,4 +1,4 @@
-import json, sys
+import argparse, json, sys
 
 def write_train_data(doc, answers):
   data = doc.get('data', {})
@@ -7,26 +7,35 @@ def write_train_data(doc, answers):
   for answer in answers:
     print(json.dumps({'completion': json.dumps(answer['data']['answer']), 'prompt': prompt}))
 
-def train(filename):
-  with open(filename) as file:
-    answers = []
-    doc = None
-    for line in file:
-      if not line.strip():
-          continue
+def train(file):
+  answers = []
+  doc = None
 
-      event = json.loads(line)
-      if event['type'] == 'document':
-        if doc:
-          write_train_data(doc, answers)
-        answers = []
-        doc = event
-      elif event['type'] == 'label-answer':
-        answers.append(event)
-  write_train_data(doc, answers)
+  for line in file:
+    if not line.strip():
+        continue
+
+    event = json.loads(line)
+    if event['type'] == 'document':
+      if doc:
+        write_train_data(doc, answers)
+      answers = []
+      doc = event
+    elif event['type'] == 'label-answer':
+      answers.append(event)
+
+  if doc:
+    write_train_data(doc, answers)
+
+def parse_args():
+  parser = argparse.ArgumentParser(description='Create OpenAI fine-tuning data from SRVC events')
+  parser.add_argument('train_file', type=str)
+  return parser.parse_args()
 
 async def main():
-  train(sys.argv[1])
+  args = parse_args()
+  with open(args.train_file) as file:
+    train(file)
 
 """
 nix run .#finetune-answers -- ../ctdbase-relations/train.jsonl > train.jsonl
